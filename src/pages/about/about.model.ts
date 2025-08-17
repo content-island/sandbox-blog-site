c3ae743f7cf17a464130cfb55e55e0377f0e214d
Braulio <braulio.diez@gmail.com>
2025-08-17 12:55:09 +0200

    completed

--- src/pages/about/about.model.ts
+++ src/pages/about/about.model.ts
@@ -1,8 +1,8 @@
-import type { Media } from '@content-island/api-client';
+import type { Media } from "@content-island/api-client";
 
 export interface About {
   id: string;
-  language: 'en';
+  language: "en";
   picture: Media;
   fullname: string;
   shortBio: string;
